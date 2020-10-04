bf417c9e6173658bdd8018c48870accf248bc432
manishbhatt151 <manishbhatt151@gmail.com>
2020-10-04 23:58:50 +0530

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int x, int y) 
+{ 
+    if (y == 0) 
+        return x; 
+    else
+        return GCD(y, x % y); 
+} 
+
+int main() {
+  cout << GCD(5, 4) << endl;
+  cout << GCD(10, 2) << endl;
+}
+
