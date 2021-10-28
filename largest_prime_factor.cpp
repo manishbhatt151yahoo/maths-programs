79fdcdb203e219d3087b95247793002934e7b534
faviforma <93350787+faviforma@users.noreply.github.com>
2021-10-29 02:09:47 +0530

    Create largest_prime_factor.cpp

---
+++ largest_prime_factor.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+long long maxPrimeFactors(long long n)
+{
+	long long maxPrime = -1;
+
+	while (n % 2 == 0) {
+		maxPrime = 2;
+		n >>= 1; 
+	}
+	while (n % 3 == 0) {
+		maxPrime = 3;
+		n=n/3;
+	}
+	for (int i = 5; i <= sqrt(n); i += 6) {
+		while (n % i == 0) {
+			maxPrime = i;
+			n = n / i;
+		}
+	while (n % (i+2) == 0) {
+			maxPrime = i+2;
+			n = n / (i+2);
+		}
+	}
+
+	if (n > 4)
+		maxPrime = n;
+
+	return maxPrime;
+}
+
+int main()
+{
+	long long n = 15;
+	cout << maxPrimeFactors(n) << endl;
+
+	n = 256926;
+	cout << maxPrimeFactors(n);
+
+}
