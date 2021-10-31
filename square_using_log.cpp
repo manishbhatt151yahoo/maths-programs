8431c60fdfc196f959907db45b90cbbacb9f7d53
Code Limner <50761731+prolimner@users.noreply.github.com>
2021-10-31 23:20:45 +0530

    Create square_using_log.cpp

---
+++ square_using_log.cpp
@@ -0,0 +1,10 @@
+// C++ program to demonstrate finding
+// square root of a number using sqrt()
+#include<bits/stdc++.h>
+
+int main(void)
+{
+	double n = 12;
+	printf("%lf ", sqrt(n));
+	return 0;
+}
